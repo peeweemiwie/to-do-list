554f9de704417066a2c7aa280c23a3629e5ae6b8
Joseph Pedicini <joey.pedicini@gmail.com>
2018-05-18 14:51:20 -0400

    import melanin

--- src/app/inputs/inputs.component.ts
+++ src/app/inputs/inputs.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inputs',
   templateUrl: './inputs.component.html',
-  styleUrls: ['./inputs.component.css']
+  styleUrls: ['./inputs.component.scss']
 })
 export class InputsComponent implements OnInit {
 
