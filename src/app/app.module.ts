e3472096312c921e64241c97691df75f75408c2c
peeweemiwie <miwayagi@gmail.com>
2018-07-31 05:40:45 -0400

    Create cancel button to clear text input field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InputsComponent } from './inputs/inputs.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     InputsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
